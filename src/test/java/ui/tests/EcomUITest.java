b08841b3aa9517d13c0c9d2b43f625d5e3491f58
cabalos <cabalosos@gmail.com>
2018-02-18 23:18:51 +0200

    add docker

--- src/test/java/ui/tests/EcomUITest.java
+++ src/test/java/ui/tests/EcomUITest.java
@@ -3,40 +3,45 @@ package ui.tests;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.BoardPage;
 import pages.AutorizationPage;
 import pages.PhotoPage;
+
+import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selenide.open;
 
 /**
  * Created by OLEX on 17.02.2018.
  */
-public class EcomUITest {
+public class EcomUITest extends BaseTestClass {
+
    private BoardPage board;
    private PhotoPage photo;
    private AutorizationPage autorization;
 
-    @Test
-    public void newTest()  {
-        System.setProperty("webdriver.chromedriver.driver", "src/main/resources/chromedriver.exe");
-        System.setProperty("selenide.browser","chromedriver");
-        ChromeDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-        driver.manage().window().maximize();
-        open("https://www.istockphoto.com");
-        autorization= new AutorizationPage();
-        board = autorization.signIn();
+    @DataProvider(name = "browser", parallel = true)
+    public Object[][] myBrowsers() {
+        return new Object[][]{{"c"}, {"f"}};
+    }
+
+    @Test(dataProvider = "browser")
+    public void newTest(String browser)  {
+        openBrowser(browser);
+        board = new BoardPage();
         photo = board.createNewBoard();
         Assert.assertEquals(board.checkBoard(),"newBoard");
         photo.copyPhotoToBoard();
         Assert.assertEquals(photo.checkPhotoIsCreated(),"asset_478130948");
         photo.deletePhoto();
-        Assert.assertEquals(photo.checkButtonAddIsDisplayed(),"0 files");
+   //     Assert.assertEquals(photo.checkButtonAddIsDisplayed(),"0 files");
         board.deleteMyBoard();
         Assert.assertEquals(board.checkBoardAreDeleted(),"Collect what you love. Start with a search.");
         board.goToMainPage();
-        autorization.signOut();
+        new AutorizationPage().signOut();
     }
 }
